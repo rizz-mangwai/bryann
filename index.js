29acac2331758a053a7b2fbb5250b51b54dc9729
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-02-01 14:43:24 +0300

    add form submit  event listener and prevent default

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-const malenames
+const daysOfWeek = ["saturday", "sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ];
+const maleAkan = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+const FemaleAkan = ["Ama",   "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
