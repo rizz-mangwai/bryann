85bebb6fd58fd8ea4282be23243395d1f2c427b7
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-02-01 15:00:45 +0300

    add year range

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ function daysinmonth(m, y){
     return [0,31,28,31,30,31,30,31,31,30,31,30,31] [m] +
     (m === 2 && (y % 400 === 0 || (y % 4 === 0 && y % 100 !== 0)) ? 1 : 0)
 }
+document.getElementById("akanForm").addEventListener("submit", function(e) {
+  e.preventDefault();
