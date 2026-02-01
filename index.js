ee7f2b0e59036ca8063a0529d77756698beacf6b
Bryan Ronoh <bryankipchumba200@gmail.com>
2026-02-01 14:54:48 +0300

    i mplement basic input parsing and empty field validation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const daysOfWeek = ["saturday", "sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", ];
 const maleAkan = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
 const FemaleAkan = ["Ama",   "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
+
+function daysinmonth(m, y){
+    return [0,31,28,31,30,31,30,31,31,30,31,30,31] [m] +
+    (m === 2 && (y % 400 === 0 || (y % 4 === 0 && y % 100 !== 0)) ? 1 : 0)
+}
